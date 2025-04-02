9ced29b735d1f0bc69bd89aff4b6e7f5e70918d3
Logan-01-for <sjson0105@gmail.com>
2025-04-02 17:46:12 +0900

    0402링크

---
+++ 3_Javascript/js/메뉴판관리.js
@@ -0,0 +1,5 @@
+// 힌트.
+// http://192.168.50.229:5500/3_Javascript/html/%EB%A9%94%EB%89%B4%ED%8C%90%EA%B4%80%EB%A6%AC.html
+// 1. input 태그가 focus를 잃었을 때 (blur) : 요소.addEventListener("blur", () => {})
+
+// 2. classList.toggle() : 지정된 클래스가 요소에 존재하면 제거하고, 존재하지 않으면 추가하는 방식
